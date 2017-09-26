742c33ddb05fe07195c641ed4c03332c3fa24d23
Agustín Ramiro Quetto Garay Lima <agustinquetto@gmail.com>
2017-09-26 19:40:47 -0300

    Test with incoherencies

---
+++ SistemaCompraVenta/SistemaCompraVenta/Articulo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCompraVenta
+{
+    public class Articulo
+    {
+        protected int _codigo;
+        protected string _nombre;
+        protected float _precioCosto;
+        protected float _precioVenta;
+        protected int _stock;
+
+        public string NombreYCodigo
+        {
+            get
+            {
+                return this._nombre + " " + this._codigo;
+            }
+        }
+
+        public float PrecioCosto
+        {
+            set
+            {
+                this._precioCosto = value;
+                this._precioVenta = value * (float)1.3;
+            }
+        }
+
+        public float PrecioVenta
+        {
+            get
+            {
+                return this._precioVenta;
+            }
+        }
+
+        public int Stock
+        {
+            set
+            {
+                this._stock = value;
+            }
+        }
+
+        public Articulo(int codigo, string nombre, float precioCosto, int cantidad)
+        {
+            this._codigo = codigo;
+            this._nombre = nombre;
+            this._precioCosto = precioCosto;
+            this._stock = cantidad;
+        }
+
+        public bool HayStock(int cantidad)
+        {
+            if (this._stock <= cantidad)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator ==(Articulo uno, Articulo dos)
+        {
+            return uno.NombreYCodigo == dos.NombreYCodigo;
+        }
+
+        public static bool operator !=(Articulo uno, Articulo dos)
+        {
+            return !(uno == dos);
+        }
+
+        public static int operator +(Articulo uno, Articulo dos)
+        {
+            int _stock = uno._stock + dos._stock;
+            return _stock;
+        }
+
+        public static int operator -(Articulo uno, int cantidad)
+        {
+            return 0;
+        }
+    }
+}
---
+++ SistemaCompraVenta/SistemaCompraVenta/Comercio.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaCompraVenta
+{
+    public class Comercio
+    {
+        protected string _dueno;
+        protected List<Articulo> _misArticulos;
+        protected List<Venta> _misVentas;
+
+        public Comercio(string dueno)
+        {
+            _dueno = dueno;
+            _misArticulos = new List<Articulo>();
+            _misVentas = new List<Venta>();
+
+        }
+
+        public static void MostrarArticulos(Comercio comercio)
+        {
+            foreach (Articulo articulo in comercio._misArticulos)
+            {
+                Console.WriteLine(articulo.NombreYCodigo);
+            }
+        }
+
+        public static void MostrarGanancia(Comercio comercio)
+        {
+            double total = 0;
+
+            foreach(Venta venta in comercio._misVentas)
+            {
+                total += venta.RetornarGanancia();
+            }
+
+            Console.WriteLine("Ganancia total de " + comercio._dueno + ": $" + total);
+        }
+
+        public void ComprarArticulo(Articulo articuloComprado)
+        {
+            bool found = false;
+            foreach (Articulo articulo in this._misArticulos)
+            {
+                if(articulo == articuloComprado)
+                {
+                    articulo.Stock = articulo + articuloComprado;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                this._misArticulos.Add(articuloComprado);
+            }
+        }
+
+        public void VenderArticulo(Articulo articulo, int cantidad)
+        {
+            foreach (Articulo articuloFor in this._misArticulos)
+            {
+                if (articuloFor == articulo && articulo.HayStock(cantidad))
+                {
+                    articulo.Stock = articuloFor - cantidad;
+                    Venta venta = new Venta(articuloFor, cantidad);
+                    break;
+                }
+                /*else
+                {
+                    Console.WriteLine("No se encuentran coincidencias o no hay stock.");
+                }*/
+            }
+                Console.WriteLine("No se encuentran coincidencias.");
+        }
+    }
+}
